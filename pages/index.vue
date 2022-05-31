8db8fd7e0d872d6d00b5558bc795a3ddb97c160b
JucoskiCristian <cristian.jucoski@gmail.com>
2022-05-31 17:35:05 -0300

    Projeto ToDoList

---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <b-container fluid>
+    <b-row align-h="center">
+      <h1>To do List</h1>
+    </b-row>
+    <b-row align-h="center">
+      <b-col cols="3" md="4">
+        <b-form-input v-model="text" placeholder="Tarefa"></b-form-input>
+        <b-button v-if="text !== ''" class="mt-2" @click="adicionarLista({nome : text})">Adicionar</b-button>
+        <b-button v-else disabled class="mt-2">Adicionar</b-button>
+        <b-alert  :show="dismissCountDown" fade dismissible variant="success" @dismiss-count-down="countDownChanged">Tarefa Cadastrada Com Sucesso!!!</b-alert>
+      </b-col>
+    </b-row>
+    <b-row align-h="center" class="m-2">
+      <b-col cols="3" md="4">
+        <b-list-group>
+          <b-list-group-item :key="item" v-model="variant" variant="info" v-for="(item , index) in tarefas">
+            <b-row align-h="center">
+              <b-col cols="9" >
+                <span>{{item.nome}}</span>
+              </b-col>
+              <b-col cols="3">
+                <b-button @click="removerLista(index)" variant="danger">Excluir</b-button>
+              </b-col>
+            </b-row>          
+          </b-list-group-item>
+        </b-list-group>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dismissSecs: 3,
+      dismissCountDown: 0,
+      showDismissibleAlert: false,
+      text: "",
+      tarefas: [],
+    };
+  },
+  methods: {
+    adicionarLista(text) {
+      this.tarefas.push(text);
+      this.text = "";
+      this.showAlert();
+    },
+    countDownChanged(dismissCountDown) {
+      this.dismissCountDown = dismissCountDown;
+    },
+    showAlert() {
+      this.dismissCountDown = this.dismissSecs;
+    },
+    removerLista(index){
+      this.tarefas.splice(index,1);
+    }
+  },
+};
+</script>
